e113bc7e4616f373d5bb9b9ca33056d66e37690e
aymen7 <aymenokay@gmail.com>
2018-08-11 14:01:31 +0100

    validate the message according to the type (need some minor adjustments)

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,6 +11,7 @@ export class FormComponent implements OnInit {
   myForm: FormGroup;
   /*post instance to store our user's input data */
   post: Post;
+  messageLength: number;
   /*inject fb instance as our FormBuilder*/
   constructor(_fb: FormBuilder) {
     this.myForm = _fb.group({
@@ -20,6 +21,7 @@ export class FormComponent implements OnInit {
       'type': ['', Validators.compose([Validators.required])],
       'terms_check_box': ['', Validators.compose([Validators.required])]
     });
+    this.messageLength = 0;
   }
   submitForm(form: any) {
     /* initialisation of the new post object with the user's input*/
@@ -31,9 +33,55 @@ export class FormComponent implements OnInit {
     console.log('form value: ' + this.post.type);
     console.log('form value: ' + form.terms_check_box);
   }
-  ngOnInit() {
+  getMsgLength() {
+    /* get the value of the message length on value changes */
+    this.myForm.get('message').valueChanges.subscribe(
+      (message) => {
+        this.messageLength = message.toString().length;
+      }
+    );
   }
-}
+  checkBoxChange() {
+    // on terms_check_box valueChange we gonna disable and enable the submit btn accordingly
+    this.myForm.get('terms_check_box').valueChanges.subscribe(
+      /*(terms_check_box) => {
+        if (terms_check_box === '1') {
+          // this.myForm.get('submit_btn').enable();
+          this.myForm.controls['submit_btn'].enable();
+        }// end of if
+        else {
+          // this.myForm.get('submit_btn').disable();
+          this.myForm.controls['submit_btn'].disabled();
+        }// end of else
+      }*/
+    );
+  }
+  getMessageType() {
+    /* validate the message based on:
+   * 1.must be longer or equal to 100 character if the type chosen is 'long'
+   * 2.must be less than 100 character if the type chosen is 'short'*/
+    this.myForm.get('type').valueChanges.subscribe(
+      (type) => {
+        if (type === 'short') {
+          this.myForm.get('message').setValidators([Validators.required, shortMessageValidator]);
+          console.log(this.myForm.get('message').value.toString().length + ' short');
+        }// end of if
+        if (type === 'long') {
+          this.myForm.get('message').setValidators([Validators.required, longMessageValidator]);
+          console.log(this.myForm.get('message').value.toString().length + ' long');
+        }// end if
+      }// fat arrow
+    );
+  }
+  ngOnInit() {
+    // calling the getMsgLength()
+    this.getMsgLength();
+    // calling the checkBoxChange
+    // this.checkBoxChange();
+    // calling getMessageType()
+    this.getMessageType();
+  }
+}// end of the component
 function usernameValidator(control: FormControl): { [s: string]: boolean} {
   /* this function is with the purpose of validating the username
      it take a FormControl instance as an input and return 'invalidUsername' if the username is not valid
@@ -45,5 +93,27 @@ if (!control.value.match(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i) ) {
   return {invalidUsername: true};
 }
 }
+function shortMessageValidator(control: FormControl): { [s: string]: boolean} {
+  /* validate the message based on:
+  * 2.must be less than 100 character if the type chosen is 'short'*/
+  if (control.value.toString().length > 100 ) {
+    this.myForm.get('message').updateValueAndValidity();
+    this.myForm.get('type').updateValueAndValidity();
+    this.myForm.updateValueAndValidity();
+    console.log('short msg validator activate');
+    return {invalidMessage: true};
+  }
 
+}
+function longMessageValidator(control: FormControl): { [s: string]: boolean} {
+  /* validate the message based on:
+  * 1.must be longer or equal to 100 character if the type chosen is 'long' */
+  if (control.value.toString().length < 100 ) {
+    this.myForm.get('message').updateValueAndValidity();
+    this.myForm.get('type').updateValueAndValidity();
+    this.myForm.updateValueAndValidity();
+    console.log('long msg validator activate');
+    return {invalidMessage: true};
+  }
+}
 
